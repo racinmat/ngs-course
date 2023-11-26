fa85233b986f7902b2d7cd9fc5011062801c4cd0
Matej Racinsky <racinsky.matej@seznam.cz>
2023-11-26 10:14:52 +0100

    updated workflow, ignoring swap

--- workflow.sh
+++ workflow.sh
@@ -2,4 +2,4 @@
 # count features per chromosome
 INPUT=data/Ensembl.NCBIM37.67.bed
 <$INPUT cut -f1 | sort | uniq -c
-<data/HRTMUOC01.RL12.00.fastq paste - - - - | awk '(length($2) > 100)' | tr '\t' '\n' | head
+<data/HRTMUOC01.RL12.00.fastq paste - - - - | awk '(length($2) > 100)' | tr '\t' '\n' | head > data2/filtered.fastq
